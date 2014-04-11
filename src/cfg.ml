d6906865d3203257d808635c79e7bb1f9218e0f5
cergo <cergo@coffee-machine.(none)>
2014-04-11 11:09:40 +0200

    Add detailed information to the labels of the CFG

--- src/cfg.ml
+++ src/cfg.ml
@@ -24,7 +24,14 @@ end
 
 module LabelMap = Map.Make (LABEL)
 
-type cfg = ((command * label) list) LabelMap.t
+type label_info = {
+  mutable loop_header: bool;                     (* Identify widening points. *)
+  mutable do_widen: bool;                        (* When to perform the widening. *)
+  mutable successors: (command * label) list;
+  mutable predecessors: label list
+}
+
+type cfg = label_info LabelMap.t
 
 
 (** Conversion of expressions to linear expressions.
@@ -56,13 +63,33 @@ let rec to_bexpr (e: expression): (linexpr * string) bexpr =
 
 (** Construction of the graph. *)
 
+(* Initialize a label in the graph.
+   The boolean flag identifies loop headers. *)
+let init_label (l: label) (cfg: cfg): cfg =
+  LabelMap.add l {
+    loop_header = false;
+    do_widen = false;
+    successors = [];
+    predecessors = []
+  } cfg
+
 (* Insert a command in the graph. *)
-let insert (l: label) (c: command * label) (cfg: cfg): cfg =
+let insert (l0: label) (c,l1: command * label) (cfg: cfg): unit =
   try
-    let cs = LabelMap.find l cfg in
-    LabelMap.add l (c::cs) cfg
+    let info0 = LabelMap.find l0 cfg
+    and info1 = LabelMap.find l1 cfg in
+    info1.predecessors <- l0::info1.predecessors;
+    info0.successors <- (c,l1)::info0.successors
   with Not_found ->
-    LabelMap.add l [c] cfg
+    assert false
+
+(* Mark a label as loop header. *)
+let set_loop_header (l: label) (cfg: cfg): unit =
+  try
+    let info = LabelMap.find l cfg in
+    info.loop_header <- true
+  with Not_found ->
+    assert false
 
 (* Current labels of destination of break and continue expressions. *)
 let break_label = ref undefined_label
@@ -78,39 +105,45 @@ let rec insert_instruction (l0: label) (l1: label) (instr: instruction) (cfg: cf
      TODO: find a way to remove the useless branching condition. *)
   | Declare (p, t, x, None) ->
       create_var x t;
-      insert l0 (Cond Top, l1) cfg
+      insert l0 (Cond Top, l1) cfg;
+      cfg
   | Declare (p, t, x, Some e) ->
       create_var x t;
       insert_instruction l0 l1 (Syntax.Assign (p, x, e)) cfg
   | Syntax.Assign (p, x, e) ->
       let x' = find_var p x in
-      insert l0 (Assign (x', to_linexpr e), l1) cfg
+      insert l0 (Assign (x', to_linexpr e), l1) cfg;
+      cfg
   | If (p, e, (p0, b0), None) ->
       let l2 = new_label (start_of_position p0) in
+      let cfg = init_label l2 cfg in
       let e = to_bexpr e in
-      let cfg = insert l0 (Cond e, l2) cfg in
-      let cfg = insert l0 (Cond (bnot rev e), l1) cfg in
+      insert l0 (Cond e, l2) cfg;
+      insert l0 (Cond (bnot rev e), l1) cfg;
       in_scope (fun _ -> insert_block l2 l1 b0 cfg)
   | If (p, e, (p0,b0), Some (p1, b1)) ->
       let l2 = new_label (start_of_position p0)
       and l3 = new_label (start_of_position p1) in
+      let cfg = init_label l3 (init_label l2 cfg) in
       let e = to_bexpr e in
-      let cfg = insert l0 (Cond e, l2) cfg in
-      let cfg = insert l0 (Cond (bnot rev e), l3) cfg in
+      insert l0 (Cond e, l2) cfg;
+      insert l0 (Cond (bnot rev e), l3) cfg;
       let cfg = in_scope (fun _ -> insert_block l3 l1 b1 cfg) in
       in_scope (fun _ -> insert_block l2 l1 b0 cfg)
   | While (p, e, (p',b)) ->
       let l2 = new_label (start_of_position p') in
+      let cfg = init_label l2 cfg in
       let e = to_bexpr e in
-      let cfg = insert l0 (Cond e, l2) cfg in
-      let cfg = insert l0 (Cond (bnot rev e), l1) cfg in
+      set_loop_header l0 cfg;
+      insert l0 (Cond e, l2) cfg;
+      insert l0 (Cond (bnot rev e), l1) cfg;
       break_label := l1;
       continue_label := l0;
       in_scope (fun _ -> insert_block l2 l0 b cfg)
   (* This case correspond to blocks that contain only one break or continue statement.
      TODO: simplify this and remove the useless branching condition. *)
-  | Break p -> insert l0 (Cond Top, !break_label) cfg
-  | Continue p -> insert l0 (Cond Top, !continue_label) cfg
+  | Break p -> insert l0 (Cond Top, !break_label) cfg; cfg
+  | Continue p -> insert l0 (Cond Top, !continue_label) cfg; cfg
 
 (* Insert a block in the graph. *)
 and insert_block (l0: label) (l1: label) (is: instruction list) (cfg: cfg): cfg =
@@ -121,6 +154,7 @@ and insert_block (l0: label) (l1: label) (is: instruction list) (cfg: cfg): cfg 
   | i::(Continue _)::_ -> insert_instruction l0 !continue_label i cfg
   | i::is ->
       let l2 = new_label (end_of_position (position_of_instruction i)) in
+      let cfg = init_label l2 cfg in
       let cfg = insert_instruction l0 l2 i cfg in
       insert_block l2 l1 is cfg
 
@@ -130,7 +164,8 @@ let build_cfg (p,b: block): cfg =
   reset_labels ();
   let l0 = new_label (start_of_position p)
   and l1 = new_label (end_of_position p) in
-  insert_block l0 l1 b LabelMap.empty
+  let cfg = init_label l0 (init_label l1 LabelMap.empty) in
+  insert_block l0 l1 b cfg
 
 
 (** Printing. *)
@@ -143,12 +178,12 @@ let string_of_command (c: command): string =
   | Assign (x,e) -> x ^ " = " ^ string_of_linexpr e
 
 let print_cfg (cfg: cfg): unit =
-  LabelMap.iter (fun l0 cs ->
+  LabelMap.iter (fun l0 info ->
     print_string (string_of_label l0 ^ ":");
     print_newline ();
     List.iter (fun (c, l1) ->
       print_string ("  " ^ string_of_command c ^ " -> " ^ string_of_label l1);
       print_newline ()
-    ) cs
+    ) info.successors
   ) cfg
 
