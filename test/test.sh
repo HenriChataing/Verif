4026c1681c38b4b7633b90ba18d8ce25ca9a233d
Henri Chataing <chataing.henri@gmail.com>
2014-05-14 13:52:12 +0200

    Modify the test script

--- test/test.sh
+++ test/test.sh
@@ -4,9 +4,6 @@
 # to have DOT output
 DOT=
 
-# Percentage of simplification
-per=0
-
 for d in $@; do
 	echo -e "\r\033[0KEntering directory $d"
 	dsize=$(ls -l $d | wc -l)
@@ -16,14 +13,13 @@ for d in $@; do
 		echo -ne "\r\033[0K[$index/$dsize] Testing $f ..."
 		r0=$(z3 $f)
 		if [ $DOT ]; then
-		  p=$(../src/verif $f --verbose 2 -d graph.dot -s tmp.smt2)
+		  p=$(../src/verif $f -d graph.dot -s tmp.smt2)
 			dot graph.dot -Tpng -o "$f.png"
 		  rm graph.dot
 		else
-			p=$(../src/verif $f --verbose 2 -s tmp.smt2)
+			p=$(../src/verif $f -s tmp.smt2)
 		fi
 		r1=$(z3 tmp.smt2)
-		per=$(echo "$per + $p" | bc)
 		if [ "$r0" != "$r1" ]; then
 			echo -e "\r\033[0K\e[1;31m$f  :  $r0 != $r1\e[0m"
 			cp $f error/
@@ -31,7 +27,5 @@ for d in $@; do
 		rm tmp.smt2
 		let "index += 1"
 	done
-	per=$(echo "$per / $dsize" | bc)
-	echo -e "\r\033[0KDone with directory $d; Average arg deletion: $per %"
 done
 echo -e "\r\033[0KDone."
